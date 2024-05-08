c3eec0a061b67520f4840c7c57791a9bc5623369
Nemanja Markovic <necamark@gmail.com>
2024-05-08 16:31:04 +0200

    created Food class; detect collision with food

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Turtle, Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -9,6 +10,7 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -22,5 +24,8 @@ while game_is_on:
     time.sleep(0.1)
 
     snake.move()
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
