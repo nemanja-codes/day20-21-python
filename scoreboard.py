fe751525fa6fc7dbce79300e775aaf8be09e0f0e
Nemanja Markovic <necamark@gmail.com>
2024-05-12 02:14:26 +0200

    improving my game; keep track of high score and save it to a file called data.txt

--- scoreboard.py
+++ scoreboard.py
@@ -7,19 +7,32 @@ class Score(Turtle):
 
     def __init__(self):
         super().__init__()
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
         self.penup()
         self.speed("fastest")
         self.color("white")
         self.hideturtle()
         self.score = 0
         self.goto(0, 270)
-        self.write(f"Score: {self.score}", False, ALIGNMENT, FONT)
+        self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", False, ALIGNMENT, FONT)
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", False, ALIGNMENT, FONT)
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", False, ALIGNMENT, FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
-        self.write(f"Score: {self.score}", False, ALIGNMENT, FONT)
+        self.update_scoreboard()
--- snake.py
+++ snake.py
@@ -26,6 +26,13 @@ class Snake:
         new_segment.goto(position)
         self.snake_parts.append(new_segment)
 
+    def reset(self):
+        for part in self.snake_parts:
+            part.goto(1000, 1000)
+        self.snake_parts.clear()
+        self.create_snake()
+        self.head = self.snake_parts[0]
+
     def extend(self):
         self.add_segment(self.snake_parts[-1].position())
 
